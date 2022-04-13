5514b7e399ee17e98a90585cad9726e85b64478e
usausa9 <k021g1269@g.neec.ac.jp>
2022-04-13 13:28:31 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+// Windowsアプリでのエントリーポイント (main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// コンソールへの文字出力
+	OutputDebugStringA("Hello,DirectX!!/n");
+
+	return 0;
+}
